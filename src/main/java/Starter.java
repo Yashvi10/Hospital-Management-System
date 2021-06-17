babca284fa23689ceb2cbd9840ed8acc901d40a4
Nadish Maredia <nd843857@dal.ca>
2021-06-17 15:50:43 +0500

    dummy project

---
+++ src/main/java/Starter.java
@@ -0,0 +1,10 @@
+public class Starter {
+
+    public static void main(String[] args) {
+        System.out.println("Working");
+    }
+
+    public String getMsg(){
+        return "Hello";
+    }
+}
