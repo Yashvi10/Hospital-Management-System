cac1c1e5fafdc8c1669c5819ac3119731562b2b6
Nadish Maredia <nd843857@dal.ca>
2021-06-20 13:08:28 +0500

    add sample db logic

---
+++ src/main/java/DbConnection.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+
+    public Connection Connect(){
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con= DriverManager.getConnection(
+                    "jdbc:mysql://db-5308.cs.dal.ca/CSCI5308_8_DEVINT","CSCI5308_8_DEVINT_USER","cWhbaAs94FR");
+
+            if(con != null) {
+                return con;
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        return con;
+    }
+}
