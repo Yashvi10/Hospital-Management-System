a93f54f2ec55447551adf0def15a2acdae181dab
Nadish Maredia <nd843857@dal.ca>
2021-07-07 00:18:22 +0500

    add test cases

--- src/test/java/BillingPageTest.java
+++ src/test/java/BillingPageTest.java
@@ -2,24 +2,32 @@ import Model.Offers;
 import Services.OfferService;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BillingPageTest {
 
     @Test
-    void calculateDiscount_right(){
+    void calculateDiscount_right()  {
         // I have pass 2 as paramter which is offer id we have offer avaiable in the db with this id so it
         // will return the amount of discount rate
-        assertEquals(20, new OfferService().isOfferValid(2));
+        assertEquals(20, new OfferService().isOfferValid(2), "Offer Id is invalid");
     }
 
     @Test
-    void calculateDiscount_notfound(){
+    void calculateDiscount_notfound()  {
         // I have pass 2245 as paramter which is offer id as this offer is not found in db so
         // will return the 0
-        assertEquals(0, new OfferService().isOfferValid(2245));
+        assertEquals(0, new OfferService().isOfferValid(2245), "Offer Id is invalid");
     }
 
+    @Test
+    void getAllOffer()  {
+        OfferService offerService = new OfferService();
+        List<Offers> offersList = offerService.getAllOffer();
+        assertEquals(true, offersList.size() > 0, "Offer donot fetch successfully");
+    }
 
 
 }
--- src/test/java/PharmacyPageTest.java
+++ src/test/java/PharmacyPageTest.java
@@ -8,7 +8,8 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PharmacyPageTest {
 
     @Test
-    void isItemInList_true(){
+    void isItemInList_true()  {
+
         PharmacyPage pharmacyPage = new PharmacyPage();
         Pharmacy P1 = new Pharmacy(1, "P1", 2.0, 25, "2025-05-02");
         Pharmacy P2 = new Pharmacy(2, "P2", 4.0, 25, "2024-05-12");
@@ -16,72 +17,119 @@ public class PharmacyPageTest {
         pharmacyPage.listOfMedicines.add(P2);
 
         //Pid 1 is available in the list return true
-        assertEquals(true, pharmacyPage.isItemInList("1"), "The product is not in the list");
+        assertEquals(true, pharmacyPage.isItemInList("1"),
+                "The product is not in the list");
     }
 
     @Test
-    void isItemInList_false(){
+    void isItemInList_false()  {
+
         PharmacyPage pharmacyPage = new PharmacyPage();
-        Pharmacy P1 = new Pharmacy(1, "P1", 2.0, 25, "2025-05-02");
-        Pharmacy P2 = new Pharmacy(2, "P2", 4.0, 25, "2024-05-12");
+        Pharmacy P1 = new Pharmacy(1,
+                "P1",
+                2.0,
+                25,
+                "2025-05-02");
+        Pharmacy P2 = new Pharmacy(2,
+                "P2",
+                4.0,
+                25,
+                "2024-05-12");
         pharmacyPage.listOfMedicines.add(P1);
         pharmacyPage.listOfMedicines.add(P2);
 
         //Pid 6 is not in the list return false
-        assertEquals(false, pharmacyPage.isItemInList("6"), "The product is not in the list");
+        assertEquals(false, pharmacyPage.isItemInList("6"),
+                "The product is not in the list");
     }
 
     @Test
-    void addItemInCart_true(){
+    void addItemInCart_true()  {
+
         PharmacyPage pharmacyPage = new PharmacyPage();
-        Pharmacy P1 = new Pharmacy(1, "P1", 2.0, 25, "2025-05-02");
-        Pharmacy P2 = new Pharmacy(2, "P2", 4.0, 25, "2024-05-12");
+        Pharmacy P1 = new Pharmacy(1,
+                "P1",
+                2.0,
+                25,
+                "2025-05-02");
+        Pharmacy P2 = new Pharmacy(2,
+                "P2",
+                4.0,
+                25,
+                "2024-05-12");
         pharmacyPage.listOfMedicines.add(P1);
         pharmacyPage.listOfMedicines.add(P2);
 
         // there are two items avaiable to buy we want to buy item with id 1 and if that item is present in the list
         // it will be added in the cart
         pharmacyPage.addItemInCart("1");
-        assertEquals(true, pharmacyPage.cart.size() > 0);
+        assertEquals(true, pharmacyPage.cart.size() > 0, "There is no item in the cart");
     }
 
     @Test
-    void addItemInCart_false(){
+    void addItemInCart_false()  {
+
         PharmacyPage pharmacyPage = new PharmacyPage();
-        Pharmacy P1 = new Pharmacy(1, "P1", 2.0, 25, "2025-05-02");
-        Pharmacy P2 = new Pharmacy(2, "P2", 4.0, 25, "2024-05-12");
+        Pharmacy P1 = new Pharmacy(1,
+                "P1",
+                2.0,
+                25,
+                "2025-05-02");
+        Pharmacy P2 = new Pharmacy(2,
+                "P2",
+                4.0,
+                25,
+                "2024-05-12");
         pharmacyPage.listOfMedicines.add(P1);
         pharmacyPage.listOfMedicines.add(P2);
 
         // there are two items avaiable to buy we want to buy item with id 9989 and if that item is not present in the list
         // it will not added in the cart
         pharmacyPage.addItemInCart("9989");
-        assertEquals(false, pharmacyPage.cart.size() > 0);
+        assertEquals(false, pharmacyPage.cart.size() > 0, "Cart is empty");
     }
 
     @Test
-    public void updateStock_true(){
+    public void updateStock_true()  {
+
         PharmacyPage pharmacyPage = new PharmacyPage();
-        Pharmacy P1 = new Pharmacy(1, "P1", 2.0, 25, "2025-05-02");
-        Pharmacy P2 = new Pharmacy(2, "P2", 4.0, 25, "2024-05-12");
+        Pharmacy P1 = new Pharmacy(1,
+                "P1",
+                2.0,
+                25,
+                "2025-05-02");
+        Pharmacy P2 = new Pharmacy(2,
+                "P2",
+                4.0,
+                25,
+                "2024-05-12");
         pharmacyPage.listOfMedicines.add(P1);
         pharmacyPage.listOfMedicines.add(P2);
 
         // return true of medicine with that pid in this case "2" is in the list and update the stock by -1
-        assertEquals(true, pharmacyPage.updateStock("2"));
+        assertEquals(true, pharmacyPage.updateStock("2"),"Medicine with that id is not found");
 
     }
 
     @Test
-    public void updateStock_false(){
+    public void updateStock_false()  {
+
         PharmacyPage pharmacyPage = new PharmacyPage();
-        Pharmacy P1 = new Pharmacy(1, "P1", 2.0, 25, "2025-05-02");
-        Pharmacy P2 = new Pharmacy(2, "P2", 4.0, 25, "2024-05-12");
+        Pharmacy P1 = new Pharmacy(1,
+                "P1",
+                2.0,
+                25,
+                "2025-05-02");
+        Pharmacy P2 = new Pharmacy(2,
+                "P2",
+                4.0,
+                25,
+                "2024-05-12");
         pharmacyPage.listOfMedicines.add(P1);
         pharmacyPage.listOfMedicines.add(P2);
 
         // return false of medicine with that pid in this case "22" is not in the list
-        assertEquals(false, pharmacyPage.updateStock("22"));
+        assertEquals(false, pharmacyPage.updateStock("22"), "Medicine with that id not found");
 
     }
 }
