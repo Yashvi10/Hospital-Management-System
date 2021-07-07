651449c80abedf79b59def0065aeb419fd8c2867
Yashvi <ys472108@dal.ca>
2021-07-07 23:50:21 +0530

    Updated functions

--- src/main/java/BloodBank.java
+++ src/main/java/BloodBank.java
@@ -1,98 +1,132 @@
-import java.math.*;
 import java.sql.*;
-import java.util.Scanner;
+import java.util.*;
 
 public class BloodBank {
+
+    public static double contact = 0;
+    public static String firstname = null;
+    public static String lastname = null;
+    public static String middlename = null;
+    public static String blood_group = null;
+    public static String pid;
+    public static Scanner input;
+
+    public static String pid(){
+        input = new Scanner(System.in);
+        System.out.println("Enter your personal identification number: ");
+        pid = input.next();
+        return pid;
+    }
+
+    public static String firstname(){
+        input = new Scanner(System.in);
+        System.out.println("Enter your firstname: ");
+        firstname = input.next();
+        return firstname;
+    }
+
+    public static String middlename(){
+        input = new Scanner(System.in);
+        System.out.println("Enter your middlename: ");
+        middlename = input.next();
+        return middlename;
+    }
+
+    public static String lastname(){
+        input = new Scanner(System.in);
+        System.out.println("Enter your lastname: ");
+        lastname = input.next();
+        return lastname;
+    }
+
+    public static String blood_group(){
+        input = new Scanner(System.in);
+        System.out.println("Enter blood group: ");
+        blood_group = input.next();
+        return blood_group;
+    }
+
+    public static Double contact(){
+        input = new Scanner(System.in);
+        System.out.println("Enter your contact details: ");
+        contact = input.nextDouble();
+        return contact;
+    }
+
     public static void main(String args[]) throws SQLException {
-        PreparedStatement ps = null;
-        Connection connection = null;
-        try
-        {
-            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
-            System.out.println("JDBC Driver connection successfull.");
-        }
 
-        //handles jdbc connection errors
-        catch (Exception ex) {
+        BloodBank b = new BloodBank();
 
-            System.out.println("Error in jdbc Driver");
-        }
-        // Establish a connection
-        try {
-            String LocalURL = "jdbc:mysql://db-5308.cs.dal.ca:3306/CSCI5308_8_DEVINT";
+//        PreparedStatement ps = null;
+//        Connection connection = null;
+//        try
+//        {
+//            Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
+//            System.out.println("JDBC Driver connection successfull.");
+//        }
+//
+//        //handles jdbc connection errors
+//        catch (Exception ex) {
+//
+//            System.out.println("Error in jdbc Driver");
+//        }
+//        // Establish a connection
+//        try {
+//            String LocalURL = "jdbc:mysql://db-5308.cs.dal.ca:3306/CSCI5308_8_DEVINT";
+//
+//            connection = DriverManager.getConnection(LocalURL, "CSCI5308_8_DEVINT_USER", "cWhbaAs94FR");
+//            Statement stmt = connection.createStatement();
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        }
 
-            connection = DriverManager.getConnection(LocalURL, "CSCI5308_8_DEVINT_USER", "cWhbaAs94FR");
-            Statement stmt = connection.createStatement();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
-        int choice;
-        BigDecimal contact;
-        String firstname,lastname,middlename, blood_group;
-        double pid;
-        Scanner input = new Scanner(System.in);
         System.out.println("1 = Blood Request\n2 = Blood Donate");
         System.out.println("Select one: 1 or 2");
-        choice = input.nextInt();
+
+        input = new Scanner(System.in);
+        int choice = input.nextInt();
 
         switch(choice){
             case 1 :
                 System.out.println("You selected request for blood option!");
-                System.out.println("Enter your personal identification number: ");
-                pid = input.nextDouble();
-                System.out.println("Enter your firstname: ");
-                firstname = input.next();
-                System.out.println("Enter your middlename: ");
-                middlename = input.next();
-                System.out.println("Enter your lastname: ");
-                lastname = input.next();
-                System.out.println("Enter required blood group: ");
-                blood_group = input.next();
-                System.out.println("Enter your contact details: ");
-                contact = input.nextBigDecimal();
-                ps = connection.prepareStatement("insert into blood_request(PIN, First_Name, Middle_Name, Last_Name,Blood_Group,Contact) values(?,?,?,?,?,?)");
-                ps.setDouble(1,pid);
-                ps.setString(2, firstname);
-                ps.setString(3,middlename);
-                ps.setString(4,lastname);
-                ps.setString(5,blood_group);
-                ps.setBigDecimal(6,contact);
-                ps.executeUpdate();
+                b.pid();
+                b.firstname();
+                b.middlename();
+                b.lastname();
+                b.blood_group();
+                b.contact();
+//                ps = connection.prepareStatement("insert into blood_request(PIN, First_Name, Middle_Name, Last_Name,Blood_Group,Contact) values(?,?,?,?,?,?)");
+//                ps.setDouble(1,pid);
+//                ps.setString(2, firstname);
+//                ps.setString(3,middlename);
+//                ps.setString(4,lastname);
+//                ps.setString(5,blood_group);
+//                ps.setBigDecimal(6,contact);
+//                ps.executeUpdate();
                 break;
 
             case 2 :
                 System.out.println("You selected blood donation");
-                System.out.println("Enter your personal identification number: ");
-                pid = input.nextDouble();
-                System.out.println("Enter your firstname: ");
-                firstname = input.next();
-                System.out.println("Enter your middlename: ");
-                middlename = input.next();
-                System.out.println("Enter your lastname: ");
-                lastname = input.next();
-                System.out.println("Enter your blood group: ");
-                blood_group = input.next();
-                System.out.println("Enter your contact details: ");
-                contact = input.nextBigDecimal();
-                ps = connection.prepareStatement("insert into blood_donation(PIN, First_Name, Middle_Name, Last_Name,Blood_Group,Contact) values(?,?,?,?,?,?)");
-                ps.setDouble(1,pid);
-                ps.setString(2, firstname);
-                ps.setString(3,middlename);
-                ps.setString(4,lastname);
-                ps.setString(5,blood_group);
-                ps.setBigDecimal(6,contact);
-                ps.executeUpdate();
+                b.pid();
+                b.firstname();
+                b.middlename();
+                b.lastname();
+                b.blood_group();
+                b.contact();
+//                ps = connection.prepareStatement("insert into blood_donation(PIN, First_Name, Middle_Name, Last_Name,Blood_Group,Contact) values(?,?,?,?,?,?)");
+//                ps.setDouble(1,pid);
+//                ps.setString(2, firstname);
+//                ps.setString(3,middlename);
+//                ps.setString(4,lastname);
+//                ps.setString(5,blood_group);
+//                ps.setBigDecimal(6,contact);
+//                ps.executeUpdate();
                 break;
 
             default :
-                System.out.println("Invalid input");
+                System.out.println("Your input is not valid. Check for valid input!");
                 break;
 
         }
-
-
-
-
     }
 }
