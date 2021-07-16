5b22b03984abd9b597b7fba3f574327cb09d47b8
KushangMistry <kushangmistry283@gmail.com>
2021-07-16 18:51:37 +0530

    Vaccine Model class created

---
+++ src/main/java/Model/Vaccine.java
@@ -0,0 +1,12 @@
+package Model;
+
+/*
+ *  Name of file: Vaccine.java
+ *  Author:  Kushang Mistry
+ *  Purpose: Lists name of all approved vaccines
+ *  Description: The class is responsible to fetch all details about available vaccines
+ *               and total available doses
+ * */
+public class Vaccine
+{
+}
