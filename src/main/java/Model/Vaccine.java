779cef2efa1011de38c333a534405103e3834407
KushangMistry <kushangmistry283@gmail.com>
2021-07-16 19:02:01 +0530

    Getter and Setter created

--- src/main/java/Model/Vaccine.java
+++ src/main/java/Model/Vaccine.java
@@ -7,6 +7,40 @@ package Model;
  *  Description: The class is responsible to fetch all details about available vaccines
  *               and total available doses
  * */
-public class Vaccine
-{
+public class Vaccine {
+
+    private Integer vaccineId;
+
+    private String vaccineName;
+
+    private Integer availableDoses;
+
+    public Vaccine(String vaccineName, Integer availableDoses) {
+        this.vaccineName = vaccineName;
+        this.availableDoses = availableDoses;
+    }
+
+    public Integer getVaccineId() {
+        return vaccineId;
+    }
+
+    public void setVaccineId(Integer vaccineId) {
+        this.vaccineId = vaccineId;
+    }
+
+    public String getVaccineName() {
+        return vaccineName;
+    }
+
+    public void setVaccineName(String vaccineName) {
+        this.vaccineName = vaccineName;
+    }
+
+    public Integer getAvailableDoses() {
+        return availableDoses;
+    }
+
+    public void setAvailableDoses(Integer availableDoses) {
+        this.availableDoses = availableDoses;
+    }
 }
