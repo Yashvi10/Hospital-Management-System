aa030240232e7b69a0aa7d92f64f212573fd1197
KushangMistry <kushangmistry283@gmail.com>
2021-07-16 22:25:24 +0530

    Logic to retrieve vaccine data added.

--- src/main/java/Service/VaccineService.java
+++ src/main/java/Service/VaccineService.java
@@ -4,6 +4,9 @@ import Interface.VaccineDAO;
 import Model.Vaccine;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,10 +20,41 @@ public class VaccineService implements VaccineDAO {
 
     @Override
     public List<Vaccine> getVaccines() {
+
         CustomConnection customConnection = new CustomConnection();
         Connection dbConnection = customConnection.Connect();
 
         List<Vaccine> vaccineList = new ArrayList<Vaccine>();
-        return null;
+
+        if(dbConnection != null) {
+            String fetchQuery = "select * from vaccine";
+            Statement queryStatement = null;
+            try {
+                queryStatement = dbConnection.createStatement();
+                ResultSet resultSet = queryStatement.executeQuery(fetchQuery);
+
+                while (resultSet.next()) {
+                    Integer vaccineId = resultSet.getInt(1);
+                    String vaccineName = resultSet.getString(2);
+                    Integer availableDoses = resultSet.getInt(3);
+
+                    Vaccine vaccineData = new Vaccine(vaccineId, vaccineName, availableDoses);
+                    vaccineList.add(vaccineData);
+                }
+            }
+            catch (SQLException exception) {
+                exception.printStackTrace();
+            }
+            finally {
+                if (dbConnection != null) {
+                    try {
+                        dbConnection.close();
+                    } catch (SQLException e) {}
+                    dbConnection = null;
+                }
+            }
+        }
+
+        return vaccineList;
     }
 }
