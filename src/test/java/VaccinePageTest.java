cf77c804d1f70f81ace8990683e90ee2c38a181a
KushangMistry <kushangmistry283@gmail.com>
2021-07-18 20:27:37 -0300

    VaccinePageTest class created

---
+++ src/test/java/VaccinePageTest.java
@@ -0,0 +1,9 @@
+/*
+ *  Name of file: VaccinePageTest.java
+ *  Author:  Kushang Mistry
+ *  Purpose: To test the logic and correctness of VaccinePage class
+ *  Description: class is used for testing the logic related to pharmacy like addItemInCart(), updateStock()
+ * */
+public class VaccinePageTest {
+
+}
