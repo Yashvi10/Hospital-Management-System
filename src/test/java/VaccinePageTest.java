77a48b94960c55977040dc817173a3392e61a2bc
Kushang Mistry <ks413555@dal.ca>
2021-07-20 15:40:17 -0300

    Test method, tests vaccine availability

--- src/test/java/VaccinePageTest.java
+++ src/test/java/VaccinePageTest.java
@@ -1,9 +1,30 @@
+import org.junit.jupiter.api.Test;
+import Service.VaccineService;
+import Model.Vaccine;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /*
  *  Name of file: VaccinePageTest.java
  *  Author:  Kushang Mistry
  *  Purpose: To test the logic and correctness of VaccinePage class
- *  Description: class is used for testing the logic related to pharmacy like addItemInCart(), updateStock()
+ *  Description: This class tests logic related to vaccine feature
+ *  It also checks the correctness of the data and availability of data
  * */
 public class VaccinePageTest {
 
+    /*
+     * This method tests availability of data and boundary conditions
+     * If database has no vaccine details this test case will fail
+     *
+     * The test checks weather database has at-least one vaccine detail
+     */
+    @Test
+    void checkVaccineAvailability()
+    {
+        List<Vaccine> vaccineList = new VaccineService().getVaccines();
+        assertEquals(true, vaccineList.size() > 0);
+    }
 }
