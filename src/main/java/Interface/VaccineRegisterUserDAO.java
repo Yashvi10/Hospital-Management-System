9cc2a5d64d6bb8f737a299301cd64347f64eb671
Kushang Mistry <ks413555@dal.ca>
2021-07-20 17:59:39 -0300

    Interface Created - Operations RegisteringUser for Vaccination

---
+++ src/main/java/Interface/VaccineRegisterUserDAO.java
@@ -0,0 +1,12 @@
+package Interface;
+
+/*
+ *  Name of file: VaccineRegisterUser.java
+ *  Author:  Kushang Mistry
+ *  Purpose: An Interface having method to Register User for vaccination
+ *  Description: The Interface is responsible for DB operations related User Registration for Vaccination
+ * */
+public interface VaccineRegisterUserDAO {
+
+
+}
