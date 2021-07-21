6f6e46355ffbdf6a5d3e583b9cb54b187823aaba
Kushang Mistry <ks413555@dal.ca>
2021-07-21 03:13:45 -0300

    Logic to fetch vaccine data for a user is added

--- src/main/java/Service/VaccineRegistration.java
+++ src/main/java/Service/VaccineRegistration.java
@@ -3,9 +3,9 @@ package Service;
 import Interface.VaccineRegisterUserDAO;
 import Model.VaccineUserInformation;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /*
  *  Name of file: VaccineRegistration.java
@@ -53,7 +53,42 @@ public class VaccineRegistration implements VaccineRegisterUserDAO {
   }
 
   @Override
-  public Boolean getUserInfo (Integer userId) {
-    return false;
+  public List<VaccineUserInformation> getUserInfo(Integer userId) {
+
+    CustomConnection customConnection = new CustomConnection();
+    Connection conn = customConnection.Connect();
+
+    List<VaccineUserInformation> userVaccineDetails = new ArrayList<>();
+
+    if (conn != null) {
+      String fetchQuery = "select * from vaccination_patients where userId = "+userId;
+      Statement queryStatement = null;
+      try {
+        queryStatement = conn.createStatement();
+        ResultSet resultSet = queryStatement.executeQuery(fetchQuery);
+
+        while (resultSet.next()) {
+          String mailId = resultSet.getString("mail_id");
+          Integer age = resultSet.getInt("age");
+          String gender = resultSet.getString("gender");
+          String governmentId = resultSet.getString("government_id_number");
+          Date date = resultSet.getDate("preferred_next_date");
+          String preferred_timing = resultSet.getString("preferred_timing");
+
+          VaccineUserInformation userInformation = new VaccineUserInformation(userId, mailId, age, gender, governmentId,date, preferred_timing);
+          userVaccineDetails.add(userInformation);
+        }
+      } catch (SQLException exception) {
+        exception.printStackTrace();
+      } finally {
+        if (conn != null) {
+          try {
+            conn.close();
+          } catch (SQLException e) {
+          }
+        }
+      }
+    }
+    return userVaccineDetails;
   }
 }
