a090a20fb13704e7782db2e84e04b3b5799593a5
Kushang Mistry <ks413555@dal.ca>
2021-07-22 14:41:14 -0300

    Getting started with Help Desk Feature

---
+++ src/main/java/View/HelpDeskPage.java
@@ -0,0 +1,19 @@
+package View;
+
+import Interface.FeatureMenu;
+
+/*
+ *  Name of file: HelpDeskPage.java
+ *  Author:  Kushang Mistry
+ *  Purpose: The primary class which provides all Help Desk related options to end-user
+ *  Description: The class calls methods based on user's choice
+ *               Sub-menu (Vaccine menu) iterates itself until user selects
+ * */
+public class HelpDeskPage implements FeatureMenu {
+
+  // Method which is responsible to call Help Desk Menu (Sub-menu of the system)
+  @Override
+  public void menu() {
+
+  }
+}
