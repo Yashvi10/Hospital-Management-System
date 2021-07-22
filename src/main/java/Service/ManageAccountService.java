eb092a9f8ee592292acfd0e784776426b2a75441
ab338210 <babalola>
2021-07-22 07:11:25 +0100

    Model Accounts.java created

--- src/main/java/Service/ManageAccountService.java
+++ src/main/java/Service/ManageAccountService.java
@@ -17,7 +17,6 @@ import java.util.List;
 
 public class ManageAccountService  implements IAccount {
 
-  Statement statement = null;
   ResultSet resultSet = null;
   DatabaseService dbService;
   PreparedStatement insertAccounts;
