d2e5756869553b2c5d2d08f3ebb4575ae952522d
Kushang Mistry <ks413555@dal.ca>
2021-07-22 21:36:53 -0300

    Test class created for Help Desk

--- src/test/java/HelpDeskPageTest.java
+++ src/test/java/HelpDeskPageTest.java
@@ -16,14 +16,4 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * */
 public class HelpDeskPageTest {
 
-  /*
-   * Tests the logic of description input validation
-   *
-   * The method returns true as description is valid.
-   */
-  @Test
-  void  checkValidateDescription_true() {
-    HelpDeskPage helpDeskPage = new HelpDeskPage();
-    assertEquals(true, helpDeskPage.validateDescription("When will I get vaccine?"));
-  }
 }
