5b2f2b66d611aafa97d755f0cbb848b958ec10b7
Kushang Mistry <ks413555@dal.ca>
2021-07-22 21:36:33 -0300

    Test class created for Help Desk

---
+++ src/test/java/HelpDeskPageTest.java
@@ -0,0 +1,29 @@
+import BL.HelpDeskRequestRegisterBL;
+import BL.VaccineRegisterBL;
+import Service.HelpDeskRegisterService;
+import Service.VaccineRegistration;
+import View.HelpDeskPage;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/*
+ *  Name of file: HelpDeskPageTest.java
+ *  Author:  Kushang Mistry
+ *  Purpose: This class tests operations related to Help Desk
+ *  Description: These test cases ensures how the logic should behave
+ *               Passing all test cases ensures that logic is working as intended.
+ * */
+public class HelpDeskPageTest {
+
+  /*
+   * Tests the logic of description input validation
+   *
+   * The method returns true as description is valid.
+   */
+  @Test
+  void  checkValidateDescription_true() {
+    HelpDeskPage helpDeskPage = new HelpDeskPage();
+    assertEquals(true, helpDeskPage.validateDescription("When will I get vaccine?"));
+  }
+}
