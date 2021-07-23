17649debeed249a2240f30d8f5fa0a19875e451c
Kushang Mistry <ks413555@dal.ca>
2021-07-23 20:31:05 -0300

    Interface added for Business Logic

---
+++ src/main/java/Interface/CovidBedBLInterface.java
@@ -0,0 +1,14 @@
+package Interface;
+
+import Model.HelpDeskRequestInformation;
+
+/*
+ *  Name of file: CovidBedBLInterface.java
+ *  Author:  Kushang Mistry
+ *  Description: The Interface is responsible for declaration of methods which should be implemented in Business logic
+ *  Purpose: An Interface having methods of Business Logic to view bed info, register for bed.
+ * */
+public interface CovidBedBLInterface {
+
+  Boolean getBedCount(Integer userChoice);
+}
