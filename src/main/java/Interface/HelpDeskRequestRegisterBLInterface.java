3ae5a5e6eb4f2a05a61920b7284bf9595aa67232
Kushang Mistry <ks413555@dal.ca>
2021-07-22 21:32:41 -0300

    Interface for business logic created

---
+++ src/main/java/Interface/HelpDeskRequestRegisterBLInterface.java
@@ -0,0 +1,17 @@
+package Interface;
+
+import Model.HelpDeskRequestInformation;
+import Model.VaccineUserInformation;
+
+/*
+ *  Name of file: HelpDeskRequestRegisterBLInterface.java
+ *  Author:  Kushang Mistry
+ *  Description: The Interface is responsible for operations related to Help Desk request registration.
+ *  Purpose: An Interface having methods of Business Logic to Register request.
+ * */
+public interface HelpDeskRequestRegisterBLInterface {
+
+  Boolean registerRequest(HelpDeskRequestInformation helpDeskRequestInformation);
+
+  Boolean getRequestInformation(String description);
+}
