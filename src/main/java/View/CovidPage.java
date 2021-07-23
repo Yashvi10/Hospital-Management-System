11cdfe4c057ea7947b3420c6764f5ee8f20fdf6b
Kushang Mistry <ks413555@dal.ca>
2021-07-23 19:35:12 -0300

    Getting started with Covid section

---
+++ src/main/java/View/CovidPage.java
@@ -0,0 +1,13 @@
+package View;
+
+/*
+ *  Name of file: CovidPage.java
+ *  Author:  Kushang Mistry
+ *  Purpose: The primary class which provides all Covid Section related options to end-user
+ *  Description: The class calls methods based on user's choice
+ *               Sub-menu (Covid menu) iterates itself until user selects
+ * */
+public class CovidPage {
+
+
+}
